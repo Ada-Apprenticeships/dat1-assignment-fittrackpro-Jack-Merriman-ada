53c183b9b57509aa7f412c1238c5d366067a532e
Jack-Merriman-ada <jack.merriman@ada.ac.uk>
2025-02-02 15:09:12 +0000

    user_management.sql queries working

--- src/class_scheduling.sql
+++ src/class_scheduling.sql
@@ -8,12 +8,24 @@
 
 -- 1. List all classes with their instructors
 -- TODO: Write a query to list all classes with their instructors
+-- SELECT cl.name AS class_name, s.first_name ||' '|| s.last_name AS instructor_name -- use ||' '|| to join names together into one attribute
+-- FROM classes cl
+-- JOIN class_schedule cs 
+-- ON cl.class_id = cs.class_id
+-- JOIN staff s 
+-- ON cs.staff_id = s.staff_id
 
 -- 2. Find available classes for a specific date
 -- TODO: Write a query to find available classes for a specific date
+SELECT cl.name AS class_name, cs.start_time ||' - '|| cs.end_time AS class_date_and_time
+FROM classes cl
+JOIN class_schedule cs 
+ON cl.class_id = cs.class_id
+
 
 -- 3. Register a member for a class
 -- TODO: Write a query to register a member for a class
+
 
 -- 4. Cancel a class registration
 -- TODO: Write a query to cancel a class registration
--- src/user_management.sql
+++ src/user_management.sql
@@ -6,20 +6,45 @@
 
 -- User Management Queries
 
--- 1. Retrieve all members
--- TODO: Write a query to retrieve all members
+-- -- 1. Retrieve all members
+-- -- TODO: Write a query to retrieve all members
+SELECT first_name ||' ' || last_name AS members
+FROM members;
 
--- 2. Update a member's contact information
--- TODO: Write a query to update a member's contact information
+-- -- 2. Update a member's contact information
+-- -- TODO: Write a query to update a member's contact information
+UPDATE members
+SET email='alice.j1@email.com', phone_number='555-1112'
+WHERE member_id = 1 OR email='alice.j@email.com'; -- included email for a 2nd option as is also a unique key
 
--- 3. Count total number of members
--- TODO: Write a query to count the total number of members
+-- -- 3. Count total number of members
+-- -- TODO: Write a query to count the total number of members
+SELECT COUNT(member_id) AS total_members
+FROM members
 
--- 4. Find member with the most class registrations
--- TODO: Write a query to find the member with the most class registrations
+-- -- 4. Find member with the most class registrations
+-- -- TODO: Write a query to find the member with the most class registrations
+SELECT m.member_id, m.first_name, m.last_name, COUNT(ca.class_attendance_id) AS registration_count  
+FROM class_attendance ca  
+JOIN members m ON ca.member_id = m.member_id  
+WHERE ca.attendance_status = 'Registered'  
+GROUP BY m.member_id, m.first_name, m.last_name
+ORDER BY registration_count DESC  
+LIMIT 1; 
 
--- 5. Find member with the least class registrations
--- TODO: Write a query to find the member with the least class registrations
+-- -- 5. Find member with the least class registrations
+-- -- TODO: Write a query to find the member with the least class registrations
+SELECT m.member_id, m.first_name, m.last_name, COUNT(ca.class_attendance_id) AS registration_count  
+FROM class_attendance ca  
+JOIN members m ON ca.member_id = m.member_id  
+WHERE ca.attendance_status = 'Registered'  
+GROUP BY m.member_id, m.first_name, m.last_name
+ORDER BY registration_count ASC  
+LIMIT 1;
 
--- 6. Calculate the percentage of members who have attended at least one class
--- TODO: Write a query to calculate the percentage of members who have attended at least one class
+-- -- 6. Calculate the percentage of members who have attended at least one class
+-- -- TODO: Write a query to calculate the percentage of members who have attended at least one class
+SELECT (COUNT(DISTINCT ca.member_id) * 100.0 / COUNT(DISTINCT m.member_id)) AS percentage_attended
+FROM members m
+LEFT JOIN class_attendance ca 
+ON m.member_id = ca.member_id AND ca.attendance_status ='Attended'
