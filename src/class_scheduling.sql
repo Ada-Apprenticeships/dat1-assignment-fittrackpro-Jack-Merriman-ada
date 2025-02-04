967caf3c79c925f172c14c8b457a0befa243f050
Jack-Merriman-ada <jack.merriman@ada.ac.uk>
2025-02-04 22:46:16 +0000

    up to 4.6 done

--- src/class_scheduling.sql
+++ src/class_scheduling.sql
@@ -25,17 +25,40 @@
 -- GROUP BY cl.class_id, cl.name, cs.start_time, cs.end_time, cl.capacity; 
 
 
--- 3. Register a member for a class
--- TODO: Write a query to register a member for a class
+-- -- 3. Register a member for a class
+-- -- TODO: Write a query to register a member for a class
+-- INSERT INTO class_attendance (member_id, schedule_id, attendance_status)  
+-- VALUES (11, (SELECT schedule_id FROM class_schedule WHERE class_id = 3 AND start_time LIKE '2025-02-01%'), 'Registered');
 
-INSERT INTO class_attendance (member_id, schedule_id, attendance_status)  
-VALUES (11, (SELECT schedule_id FROM class_schedule WHERE class_id = 3 AND start_time LIKE '2025-02-01%'), 'Registered');
+-- -- 4. Cancel a class registration
+-- -- TODO: Write a query to cancel a class registration
+-- DELETE FROM class_attendance 
+-- WHERE member_id == 2 AND schedule_id == 7;
 
--- 4. Cancel a class registration
--- TODO: Write a query to cancel a class registration
+-- -- 5. List top 5 most popular classes
+-- -- TODO: Write a query to list top 5 most popular classes
+--     IT SAYS TOP 3 IN THIS FILE BUT TOP 5 INT HE README SO I HAVE DONE QUERIES FOR BOTH!!!
 
--- 5. List top 5 most popular classes
--- TODO: Write a query to list top 5 most popular classes
+-- TOP 3:
+-- SELECT cs.class_id, cl.name AS class_name, COUNT(ca.attendance_status) AS registration_count  
+-- FROM class_attendance ca  
+-- JOIN class_schedule cs ON cs.schedule_id = ca.schedule_id  
+-- JOIN classes cl ON cs.class_id = cl.class_id  
+-- WHERE ca.attendance_status = 'Registered'  
+-- GROUP BY cs.class_id, cl.name  
+-- ORDER BY registration_count DESC  
+-- LIMIT 3;
+
+-- -- TOP 5:
+-- SELECT cs.class_id, cl.name AS class_name, COUNT(ca.attendance_status) AS registration_count  
+-- FROM class_attendance ca  
+-- JOIN class_schedule cs ON cs.schedule_id = ca.schedule_id  
+-- JOIN classes cl ON cs.class_id = cl.class_id  
+-- WHERE ca.attendance_status = 'Registered'  
+-- GROUP BY cs.class_id, cl.name  
+-- ORDER BY registration_count DESC  
+-- LIMIT 5;
+
 
 -- 6. Calculate average number of classes per member
 -- TODO: Write a query to calculate average number of classes per member
